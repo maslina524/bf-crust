211bd6eda718343f55ba2170af5da672e25831b9
maslina524 <luk.kirill2804@gmail.com>
2026-05-26 23:52:53 +0300

    added less & greater ops

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -3,6 +3,7 @@ use core::ffi::{c_char, c_int, c_uint};
 unsafe extern "C" {
     unsafe fn printf(fmt: *const c_char, ...) -> c_int;
     unsafe fn getchar() -> c_int;
+    unsafe fn exit(status: i32) -> !;
 }
 
 pub unsafe fn run(ptr: *const u8, len: usize) -> () {
@@ -13,19 +14,40 @@ pub unsafe fn run(ptr: *const u8, len: usize) -> () {
         let byte = *ptr.add(i);
         match byte {
             43 => { // plus
-                mem[mem_i] = if mem[mem_i] == 255 { 0 } else { mem[mem_i] + 1 };
+                let val = unsafe { *mem.get_unchecked(mem_i) };
+                let new_val = if val == 255 { 0 } else { val + 1 };
+                unsafe { *mem.get_unchecked_mut(mem_i) = new_val };
             },
             44 => { // comma
-                mem[mem_i] = getchar() as u8;
+                let ch = getchar() as u8;
+                unsafe { *mem.get_unchecked_mut(mem_i) = ch };
             },
             45 => { // minus
-                mem[mem_i] = if mem[mem_i] == 0 { 255 } else { mem[mem_i] - 1 };
+                let val = unsafe { *mem.get_unchecked(mem_i) };
+                let new_val = if val == 0 { 255 } else { val - 1 };
+                unsafe { *mem.get_unchecked_mut(mem_i) = new_val };
             },
             46 => { // dot
-                printf("%c\0".as_ptr() as *const i8, mem[mem_i] as c_uint);
+                let val = unsafe { *mem.get_unchecked(mem_i) };
+                printf("%c\0".as_ptr() as *const i8, val as c_uint);
+            },
+            60 => { // less
+                if mem_i == 0 {
+                    printf("mem_i out of range\n\0".as_ptr() as *const i8);
+                    exit(1);
+                }
+                mem_i -= 1;
+            },
+            62 => { // greater
+                if mem_i == 255 {
+                    printf("mem_i out of range\n\0".as_ptr() as *const i8);
+                    exit(1);
+                }
+                mem_i += 1;
             },
             _ => {
                 printf("unknown symb: %c = %d\n\0".as_ptr() as *const i8, byte as c_int, byte as c_int);
+                exit(1);
             }
         }
     }
