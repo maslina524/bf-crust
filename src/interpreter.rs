8ea35477f4133b7943b221e0b359b488b7bc6f8e
maslina524 <luk.kirill2804@gmail.com>
2026-05-26 23:24:34 +0300

    added plus op

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,12 +1,23 @@
-use core::ffi::{c_char, c_int};
+use core::ffi::{c_char, c_int, c_uint};
 
 unsafe extern "C" {
     unsafe fn printf(fmt: *const c_char, ...) -> c_int;
 }
 
 pub unsafe fn run(ptr: *const u8, len: usize) -> () {
+    let mut mem = [0u8; 256];
+    let mut mem_i: usize = 0;
+
     for i in 0..len {
         let byte = *ptr.add(i);
-        printf("%c\0".as_ptr() as *const i8, byte as c_int);
+        match byte {
+            43 => { // plus
+                mem[mem_i] += 1;
+            }
+            _ => {
+                printf("unknown symb\n\0".as_ptr() as *const i8);
+            }
+        }
+        // printf("%c = %d\n\0".as_ptr() as *const i8, byte as c_int, byte as c_int);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ unsafe extern "C" {
 unsafe fn _start() -> i32 {
     printf("Brainfuck Interpreter in Crust!!!\n\0".as_ptr() as *const i8);
 
-    let source = "+++----[->++<]";
+    let source = "+-[],.";
     run(source.as_ptr() as *const u8, source.len());
 
     exit(0);
